8ba7a755f7440d9050f9dff20cca6993bc83700a
Steno-95 <lezzistefano95@gmail.com>
2025-04-10 20:45:31 +0200

    initial commit

---
+++ src/AppLayout.jsx
@@ -0,0 +1,11 @@
+function AppLayout() {
+  return (
+    <p>
+      Learning Published 21 Dec 2023 HTML & CSS foundations These languages are
+      the backbone of every website, defining structure, content, and
+      presentation. Greg Hooper
+    </p>
+  );
+}
+
+export default AppLayout;
---
+++ src/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+  return (
+    <footer className="text-center text-[11px] md:text-[15px] ">
+      Challenge by{" "}
+      <a
+        className="text-blue-950"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a className="text-blue-950" href="#">
+        Stefano Lezzi
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
